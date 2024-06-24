7cbe5f7b749b77345518c59613161a325bc2ca7d
Anthonia <ajayiolaide95@gmail.com>
2024-06-24 16:26:16 +0100

    IW-20: home page implementation

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from "react";
+import illustration from "../../assets/Illustration.svg";
+import logo from "../../assets/Logo.svg";
+import caret from "../../assets/chevron_right.svg";
 
 const Home = () => {
+  function handleclick() {
+    console.log("click");
+  }
   return (
-    <div>Home</div>
-  )
-}
+    <div className="bg-gradient-to-t from-blue-300 to-blue-200 h-screen px-24">
+      <header className="text-white-100 font-semibold mb-3 text-left flex items-center gap-2 pt-6">
+        <img className="" src={logo} />
+        <div className="text-[40px]">CloudCostIQ</div>
+      </header>
+      <hr className="border-2 border-grey-100 "></hr>
+      <div className="flex justify-between items-center pt-28">
+        <div className="w-[45%]">
+          <div className="text-white-100 font-semibold text-7xl">
+            Cloud Infrastructure Cost Analyzer
+          </div>
+          <p className="text-white-100 text-lg pt-4 pb-5">
+            Optimize Your Spending with Real-Time Insights and Smart
+            Recommendations
+          </p>
+          <button
+            onClick={handleclick}
+            className="text-white-100  px-6 py-2.5  rounded-md  bg-blue-100  hover:bg-green-100 flex gap-3 items-center"
+          >
+            <div className="text-lg">Get Started</div>
+            <img src={caret} />
+          </button>
+        </div>
+        <img className="w-2/5" src={illustration} />
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "white-100": "#ECEEF6",
+        "blue-200": "#1B2843",
+        "blue-300": "#060A12",
+        "blue-100": "#284FB4",
+        "green-100": "#14BC11",
+        "grey-100": " #4D5C6C",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
+};
